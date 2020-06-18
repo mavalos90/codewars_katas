73839c0f721f713b9b1b0a42f1a89adde8c4a123
mavalos90 <mavalosbarrios@gmail.com>
2020-06-18 16:13:49 +0200

    add simple_join_and_rank

---
+++ sql/6_kyu/simple_join_and_rank.sql
@@ -0,0 +1,22 @@
+-- SQL Basics: Simple JOIN and RANK
+-- 6 kyu
+
+-- For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the sales table so that you can return the COUNT of all sales and RANK each person by their sale_count.
+-- people table schema
+-- 		id
+-- 		name
+-- sales table schema
+-- 		id
+-- 		people_id
+--		sale
+--		price
+-- You should return all people fields as well as the sale count as "sale_count" and the rank as "sale_rank".
+
+SELECT 
+    p.id AS id,
+    p.name AS name,
+    COUNT(sale) AS sale_count,
+    RANK() OVER (ORDER BY COUNT(sale)) sale_rank
+FROM people p
+JOIN sales s ON p.id = s.people_id
+GROUP BY 1,2
