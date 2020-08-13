2034ef1fcd5a99b7ac8ba3b5b6832c7a6cb79b32
mavalos90 <mavalosbarrios@gmail.com>
2020-08-13 12:24:48 +0200

    add sql_basics_group_by

---
+++ sql/7_kyu/sql_basics_group_by.sql
@@ -0,0 +1,21 @@
+/*
+SQL Basics: Simple GROUP BY
+7 kyu
+
+For this challenge you need to create a simple GROUP BY statement, you want to group all the people by their age and count the people who have the same age.
+
+people table schema
+	id
+	name
+	age
+select table schema
+	age [group by]
+	people_count (people count)
+*/
+
+
+SELECT
+	age,
+    COUNT(id) AS people_count,
+FROM people
+GROUP BY 1
