0b83a1dbfc822c54ccb97eb788c01ae846b23ccc
mavalos90 <mavalosbarrios@gmail.com>
2020-08-16 11:55:22 +0200

    add sql_bit_length

---
+++ sql/7_kyu/sql_bit_length.sql
@@ -0,0 +1,18 @@
+/*
+Easy SQL: Bit Lenght
+7 kyu
+
+Given a demographics table in the following format:
+
+** demographics table schema **
+	id
+	name
+	birthday
+	race
+you need to return the same table where all text fields (name & race) are changed to the bit length of the string.
+
+*/
+
+
+SELECT id, BIT_LENGTH(name) as name, birthday, BIT_LENGTH(race) as race
+FROM demographics;
