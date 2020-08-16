c400e6a3e567595343a1cddb5749e316fffa8df1
mavalos90 <mavalosbarrios@gmail.com>
2020-08-16 11:54:40 +0200

    add sql_basics_truncating

---
+++ sql/7_kyu/sql_basics_truncating.sql
@@ -0,0 +1,16 @@
+/*
+SQL Basics: Truncating
+7 kyu
+
+Given the following table 'decimals':
+** decimals table schema **
+	id
+	number1
+	number2
+Return a table with one column (towardzero) where the values are the result of number1 + number2 truncated towards zero
+*/
+
+SELECT 
+  trunc(number1+number2) AS towardzero
+FROM 
+  decimals
