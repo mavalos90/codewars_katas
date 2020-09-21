7c88e6a9be02418b52b4a09630c63267afeced35
mavalos90 <mavalosbarrios@gmail.com>
2020-09-21 10:37:53 +0200

    SQL: opposite_number

---
+++ sql/8_kyu/opposite_number.sql
@@ -0,0 +1,16 @@
+-- Opposite number
+-- 8 kyu
+
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+You will be given a table: opposite, with a column: number. Return a table with a column: res.
+*/
+
+SELECT -1 * number as res
+FROM opposite
