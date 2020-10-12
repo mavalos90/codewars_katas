0ff06e08b8121bc36a5907bb79efc33bfb9c13d7
mavalos90 <mavalosbarrios@gmail.com>
2020-10-12 07:16:04 +0200

    SQL: View

---
+++ sql/5_kyu/simple_view.sql
@@ -0,0 +1,30 @@
+-- SQL Basics: Simple VIEW
+-- 5 kyu
+
+/*
+For this challenge you need to create a VIEW. This VIEW is used by a sales store to give out vouches to members who have spent over $1000 in departments that have brought in more than $10000 total ordered by the members id. The VIEW must be called members_approved_for_voucher then you must create a SELECT query using the view.
+
+resultant table schema
+id
+name
+email
+total_spending
+*/
+
+CREATE VIEW members_approved_for_voucher AS
+SELECT m.id, m.name, m.email, SUM(p.price) AS total_spending
+FROM members m
+INNER JOIN sales s ON s.member_id = m.id
+INNER JOIN products p ON p.id = s.product_id
+WHERE s.department_id IN (
+  SELECT s.department_id
+  FROM sales s
+  INNER JOIN products p ON p.id = s.product_id
+  GROUP BY s.department_id
+  HAVING SUM(p.price) > 10000
+)
+GROUP BY m.id, m.name, m.email
+HAVING SUM(p.price) > 1000
+ORDER BY m.id;
+
+SELECT * FROM members_approved_for_voucher;
