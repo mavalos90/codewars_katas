ce04a2c48e2b90e1fddf1f01cd509444949fa83c
mavalos90 <mavalosbarrios@gmail.com>
2020-11-05 13:28:55 +0100

    Python: Pyramid Array

---
+++ python/6_kyu/pyramid_array.py
@@ -0,0 +1,16 @@
+# Pyramid Array
+# 6 kyu
+
+'''
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Note: the subarrays should be filled with 1s
+'''
+
+
+def pyramid(n):
+    return [[1] * (i+1) for i in range(n)]
